b21920cc0d119c1d5dcae5830b532bcaaf1a39f7
marie <marie.chaze@isen.yncrea.fr>
2022-03-21 18:13:16 +0100

    First commit

--- app/src/main/java/fr/isen/chaze/androiderestaurant/MainActivity.kt
+++ app/src/main/java/fr/isen/chaze/androiderestaurant/MainActivity.kt
@@ -1,42 +1,72 @@
 package fr.isen.chaze.androiderestaurant
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
-import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import fr.isen.chaze.androiderestaurant.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+      //  setContentView(R.layout.activity_main)
+
+
         //Toast Entrées
-        val Plats = findViewById(R.id.buttonPlats) as Button
-        val Entrees = findViewById(R.id.buttonEntrees) as Button
-        val Desserts = findViewById(R.id.buttonDessert) as Button
+     //   val Plats = findViewById(R.id.buttonPlats) as Button
+     //   val Entrees = findViewById(R.id.buttonEntrees) as Button
+     //   val Desserts = findViewById(R.id.buttonDessert) as Button
 
-        Entrees.setOnClickListener {
-            Toast.makeText(
-                this@MainActivity,
-                "Vous avez cliqué sur 'Entrées'",
-                Toast.LENGTH_SHORT
-            ).show()
+        //toast pour le bouton entrée
+/*       Entrees.setOnClickListener {
+          Toast.makeText(
+              this@MainActivity,
+              "Vous avez cliqué sur 'Entrées'",
+              Toast.LENGTH_SHORT
+          ).show()
+      }
+    //toast pour le bouton plat
+      Plats.setOnClickListener {
+          Toast.makeText(
+              this@MainActivity,
+              "Vous avez cliqué sur 'Plats'",
+              Toast.LENGTH_SHORT
+          ).show()
+      }
+
+     //toast pour le bouton dessert
+         Desserts.setOnCliistener {
+          Toast.makeText(
+              this@MainActivity,
+              "Vous avez cliqué sur 'Desserts'",
+              Toast.LENGTH_SHORT
+          ).show()
+      } */
+
+      binding.buttonEntrees.setOnClickListener{
+          val intent = Intent(this, Page_Entrees::class.java)
+          startActivity(intent)
+
+      }
+        binding.buttonPlats.setOnClickListener{
+            val intent = Intent(this, Page_Plat::class.java)
+          //  intent.putExtra() //pour avoir une page au lieu de plusieurs.
+            startActivity(intent)
+
         }
 
-        Plats.setOnClickListener {
-            Toast.makeText(
-                this@MainActivity,
-                "Vous avez cliqué sur 'Plats'",
-                Toast.LENGTH_SHORT
-            ).show()
+        binding.buttonDessert.setOnClickListener{
+            val intent = Intent(this, Page_Dessert::class.java)
+            startActivity(intent)
+
         }
 
-        Desserts.setOnClickListener {
-            Toast.makeText(
-                this@MainActivity,
-                "Vous avez cliqué sur 'Desserts'",
-                Toast.LENGTH_SHORT
-            ).show()
-        }
 
-    }
+  }
 }
---
+++ app/src/main/java/fr/isen/chaze/androiderestaurant/Page_Entrees.kt
@@ -0,0 +1,11 @@
+package fr.isen.chaze.androiderestaurant
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Page_Entrees : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_page_entrees)
+    }
+}
